73427a882609bee33780b669114e84082359be3f
Ayla-McChristie <Aidan.mcchristie@gmail.com>
2025-06-24 13:20:09 -0500

    makes bullet tracker start updated

--- scenes/bullet_tracker.gd
+++ scenes/bullet_tracker.gd
@@ -19,7 +19,7 @@ var num_of_bullets
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	boxes = [box1, box2, box3, box4, box5, box6]
-	num_of_bullets = gun.cylinder.size()
+	num_of_bullets = 7
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,13 +29,15 @@ func _process(delta: float) -> void:
 			boxes[i].color = _color_pick(gun.cylinder[i])
 			
 		for i in range(gun.cylinder.size(), 6):
-			boxes[i].color = Color.DIM_GRAY
+			boxes[i].color = EMPTY_COLOR
 			
 func _color_pick(bullet_type : Global.BulletType) -> Color:
 	match  bullet_type:
 		Global.BulletType.Normal:
-			return Color.GOLD
+			return REG_COLOR
 		Global.BulletType.Blast:
-			return Color.CRIMSON
+			return BLAST_COLOR
+		Global.BulletType.Richochet:
+			return RICOCHET_COLOR
 
-	return Color.DIM_GRAY
+	return EMPTY_COLOR

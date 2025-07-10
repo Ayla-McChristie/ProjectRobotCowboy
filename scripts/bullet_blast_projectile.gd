58563b3d8c87c22459b671888d384b5bde2cf933
Ayla-McChristie <Aidan.mcchristie@gmail.com>
2025-07-09 21:54:54 -0500

    begins work on new blast bullet design

---
+++ scripts/bullet_blast_projectile.gd
@@ -0,0 +1,42 @@
+extends Node3D
+
+@export var ATTACK_DAMAGE: float = 10
+@export var KNOCKBACK_FORCE: float = 10
+
+const SPEED = 40.00
+@onready var mesh =  $MeshInstance3D
+@onready var ray: RayCast3D = $RayCast3D
+
+var expolsion = load("res://scenes/Bullets/Bullet_Blast_Explosion.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += transform.basis * Vector3(0,0, -SPEED) * delta
+		
+
+
+func _on_damage_area_body_entered(area : Node3D) -> void:
+	var instance = expolsion.instantiate()
+	if ray.is_colliding():
+		instance.position = ray.get_collision_point()
+	else: 
+		instance.position = global_position
+	instance.emitting = true
+	get_tree().root.add_child(instance)
+	print_debug("bullet collided")
+	
+	var hitbox = area.get_node_or_null("HitboxComponent")
+	
+	if hitbox:
+		var attack = Attack.new()
+		attack.attack_damage = ATTACK_DAMAGE
+		attack.knockback_force = KNOCKBACK_FORCE
+		attack.attack_point = global_position
+		hitbox.damage(attack)
+
+	queue_free()
